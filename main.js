4c1008f8dbf1c64d53989892926c65f4641f8769
Freja <frej400d@stud.kea.dk>
2022-04-05 13:15:16 +0200

    notes

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ import { rangeColorChanger, autoExpandTextarea } from "./utils.js";
 
 rangeColorChanger();
 autoExpandTextarea();
+
+//const form = document.querySelector("form")
+
+//makes the first element in happy_hour checked
+//form.elements.happy_hour[1].checked=true;
+
+
+//console.log(form.elements)
+
+
+//console.log(form.elements.name) gives elements with name 'name'
+//console.log(form.elements.colors) gives elements with name 'colors'
+
+//.value = textcontent
+//form.elements.namedItem.value
+
+//.setAttribute("disabled", true)
+//.disabled = true
+
+//use methods
